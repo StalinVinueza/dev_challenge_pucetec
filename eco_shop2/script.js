26fb7bd020fe070f905b1b6bcce4e6dbfcbdca57
StalinVinueza <csviueza@puce.edu.ec>
2024-12-01 06:53:37 -0500

    borrado de carpetas

--- eco_shop2/script.js
+++
@@ -1,8 +0,0 @@
-const dropdown = document.querySelector('.dropdown');
-const dropdownMenu = document.querySelector('.dropdown-menu');
-
-dropdown.addEventListener('click',   
- () => {
-    dropdownMenu.classList.toggle('show');   
-
-});
--- eco_shop_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EcoShopAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'eco_shop_app'
