239d6b63b4f8f9e1d0ad2bea24b5917185b979d6
StalinVinueza <csviueza@puce.edu.ec>
2024-12-01 00:11:04 -0500

    menu

---
+++ eco_shop2/script.js
@@ -0,0 +1,8 @@
+const dropdown = document.querySelector('.dropdown');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+
+dropdown.addEventListener('click',   
+ () => {
+    dropdownMenu.classList.toggle('show');   
+
+});
